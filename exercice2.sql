941b9ef54da4ae2e18f13f31d38f842f30dd8944
YAO KOUADIO YANN EVRARD <75713073+Aymar750@users.noreply.github.com>
2023-02-21 21:59:39 +0000

    Update exercice2.sql

--- exercice2.sql
+++ exercice2.sql
@@ -84,3 +84,4 @@ SELECT c.customer_id, c.first_name, c.last_name, p.amount , p.payment_date
 FROM payment p 
 JOIN customer c ON p.customer_id = c.customer_id 
 JOIN staff s ON p.staff_id = s.staff_id;
+--Aymar Yann
